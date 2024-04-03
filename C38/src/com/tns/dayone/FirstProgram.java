80f169e486630512b403ba943de472a7dba26a9b
Abdul-Malik20 <j.abdmalik20@gmail.com>
2024-04-03 20:20:59 +0530

    First Program

--- C38/src/com/tns/dayone/FirstProgram.java
+++ C38/src/com/tns/dayone/FirstProgram.java
@@ -1,10 +1,7 @@
 package com.tns.dayone;
 
 public class FirstProgram {
-
 	public static void main(String[] args) {
-		System.out.println("Hai Hello");
-
+		System.out.println("Hello World");
 	}
-
 }
