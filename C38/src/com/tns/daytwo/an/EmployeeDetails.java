9288d139fdcb8c9aed93eb2e93c09a791a4790fb
Abdul-Malik20 <j.abdmalik20@gmail.com>
2024-04-04 19:53:02 +0530

    AN SESSION - 2

---
+++ C38/src/com/tns/daytwo/an/EmployeeDetails.java
@@ -0,0 +1,46 @@
+package com.tns.daytwo.an;
+
+public class EmployeeDetails {
+	private String name;
+	private int age;
+	private int idNo;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name; // this refer to class's words
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	public int getIdNo() {
+		return idNo;
+	}
+
+	public void setIdNo(int idNo) {
+		this.idNo = idNo;
+	}
+
+	public static void main(String[] args) {
+
+		EmployeeDetails obj = new EmployeeDetails();
+
+		obj.setName("Abdul Malik J");
+		obj.setAge(20);
+		obj.setIdNo(3118);
+
+		System.out.println("Name : " + obj.getName());
+		System.out.println("Age : " + obj.getAge());
+		System.out.println("Employee ID : " + obj.getIdNo());
+
+	}
+
+}
